378d6e196f32d5e7f623b204a0b5f0c3fb27a2b7
Kate Tashman <ktashman@broadinstitute.org>
2018-05-29 11:31:55 -0400

    out flag

--- main_ldscore.py
+++ main_ldscore.py
@@ -31,7 +31,7 @@ def parse_args():
 
     parser.add_argument('--summary-stats-files', required=False,  help = 'File(s) (already processed with munge_sumstats.py) where to apply partition LDscore, files should end with .sumstats.gz. If multiple files are used, need a comma-separated list.')
     parser.add_argument('--prefix', required=True, help = 'Prefix that will be used for the ldscore files and the regression output files.')
-    parser.add_argument('--out', required=True, help = 'Path to save the regression results.')
+    parser.add_argument('--out', required=False, help = 'Path to save the regression results.')
     parser.add_argument('--export-ldscore-path', help = 'Path to export the LDscores generated from --main-annot-rsids/genes/bed')
     parser.add_argument('--no_baseline', action='store_true', default=False, help = 'Do not condition on baseline annotations')
 
